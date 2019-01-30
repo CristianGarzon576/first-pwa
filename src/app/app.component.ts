a57c67ec46cb48061183db278a504f1cc6c5bacb
Cristian Herley Garzón Moreno <cristian.garzon1495@gmail.com>
2019-01-30 14:36:40 -0500

    Configuration of Angular PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {Subscription} from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'pwa';
+  subs: Subscription;
+
+  constructor (private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.subs = this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.subs && this.subs.unsubscribe();
+  }
 }
